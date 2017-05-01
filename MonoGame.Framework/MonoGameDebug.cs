e4f9a6d5a3a9b574fa1c24aeb5368cd7123581c0
MichaelDePiazzi <MichaelDePiazzi@users.noreply.github.com>
2017-05-01 16:18:47 +0800

    Added debug message event (for external use)

---
+++ MonoGame.Framework/MonoGameDebug.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Microsoft.Xna.Framework
+{
+    public static class MonoGameDebug
+    {
+        public static event EventHandler<string> DebugMessageLogged;
+
+        public static void LogDebugMessage(string message)
+        {
+            DebugMessageLogged?.Invoke(null, message);
+        }
+    }
+}
